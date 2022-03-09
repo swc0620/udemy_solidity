ba965ddba91966f70d4fe7d0fd253e235c5e23f0
swc0620 <pinetreewave@naver.com>
2022-03-09 14:18:21 +0900

    46. Web3 Providers

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,3 +3,5 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3'); // importing constructor function of Web3
 
+const web3 = new Web3(ganache.provider()); // provider that connects network and web3
+
