052c62171c1b39372e9b58277902c9440fcec341
swc0620 <pinetreewave@naver.com>
2022-03-09 13:09:19 +0900

    44. Installing Modules

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,5 @@
 // contract test code will go here
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3'); // importing constructor function of Web3
+
