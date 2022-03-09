8df2c19106577d1a5a20070b07ee6e05bccf0442
swc0620 <pinetreewave@naver.com>
2022-03-09 15:03:46 +0900

    50. Refactor to Async/Await

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -5,19 +5,17 @@ const Web3 = require('web3'); // importing constructor function of Web3
 
 const web3 = new Web3(ganache.provider()); // provider that connects network and web3
 
-beforeEach(() => {
+let accounts;
+beforeEach(async () => {
     // Get a list of all accounts
-    web3.eth.getAccounts()
-        .then(fetchedAccounts => {
-            console.log(fetchedAccounts);
-        });
+    accounts = await web3.eth.getAccounts();
 
     // Use one of those accounts to deploy the contract
-    
+
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-
+        console.log(accounts);
     });
 })
