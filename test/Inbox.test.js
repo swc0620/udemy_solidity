0c6b5fd3b5676f66ce201788bbd52985b296e244
swc0620 <pinetreewave@naver.com>
2022-03-09 14:43:27 +0900

    47. Testing with Mocha

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -5,3 +5,27 @@ const Web3 = require('web3'); // importing constructor function of Web3
 
 const web3 = new Web3(ganache.provider()); // provider that connects network and web3
 
+class Car {
+    park() {
+        return 'stopped';
+    }
+
+    drive() {
+        return 'vroom';
+    }
+}
+
+let car;
+beforeEach(() => {
+    car = new Car();
+})
+
+describe('Car', () => {
+    it('can park', () => {
+        assert.equal(car.park(), 'stopped');
+    });
+
+    it('can drive', () => {
+        assert.equal(car.drive(), 'vroom');
+    })
+});
