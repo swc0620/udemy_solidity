bbb6d2d3f2d05a248aa3644a17dfc0412934a2d8
swc0620 <pinetreewave@naver.com>
2022-03-09 15:02:08 +0900

    49. Fetching Accounts from Ganache

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -5,27 +5,19 @@ const Web3 = require('web3'); // importing constructor function of Web3
 
 const web3 = new Web3(ganache.provider()); // provider that connects network and web3
 
-class Car {
-    park() {
-        return 'stopped';
-    }
-
-    drive() {
-        return 'vroom';
-    }
-}
-
-let car;
 beforeEach(() => {
-    car = new Car();
-})
+    // Get a list of all accounts
+    web3.eth.getAccounts()
+        .then(fetchedAccounts => {
+            console.log(fetchedAccounts);
+        });
 
-describe('Car', () => {
-    it('can park', () => {
-        assert.equal(car.park(), 'stopped');
-    });
-
-    it('can drive', () => {
-        assert.equal(car.drive(), 'vroom');
-    })
+    // Use one of those accounts to deploy the contract
+    
 });
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+
+    });
+})
