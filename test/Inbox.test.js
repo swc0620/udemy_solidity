beecf36b4b0e9c11e74a046c03d6af3184b025a7
swc0620 <pinetreewave@naver.com>
2022-03-10 11:24:14 +0900

    54. Verifying the Initial Message

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -7,18 +7,24 @@ const { interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
+const INITIAL_STRING = 'Hi there!'
 beforeEach(async () => {
     // Get a list of all accounts
     accounts = await web3.eth.getAccounts();
 
     // Use one of those accounts to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface)) // ABI as argument to constructor function
-        .deploy({ data: bytecode, arguments: ['Hi there!'] }) // contract bytecode and initialMessage
+        .deploy({ data: bytecode, arguments: [INITIAL_STRING] }) // contract bytecode and initialMessage
         .send({ from: accounts[0], gas: '1000000' }); // sender account address and gas limit
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
         assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, INITIAL_STRING);
     });
 })
