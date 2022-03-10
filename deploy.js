e88ae7ceccebe7c377db31e077d43f9ae5f897ea
swc0620 <pinetreewave@naver.com>
2022-03-10 12:33:45 +0900

    60. Wallet Provider Setup

--- deploy.js
+++ deploy.js
@@ -1,1 +1,10 @@
 // deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    process.env.mnemonic,
+    process.env.rinkebyNetwork
+);
+const web3 = new Web3(provider);
