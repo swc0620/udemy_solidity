2f481bcf2e319df33951162e8bf2617d0b06fa35
swc0620 <pinetreewave@naver.com>
2022-03-13 18:39:10 +0900

    90. Test Helper Review

---
+++ lottery_contract/deploy.js
@@ -0,0 +1,29 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const path  = require('path');
+const fs = require('fs');
+const envPath = path.resolve(__dirname, '.env');
+let source = fs.readFileSync(envPath, 'utf8');
+source = source.split("\n");
+
+const provider = new HDWalletProvider(
+  source[0],
+  source[1]
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
