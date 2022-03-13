913f21e67875e493f3dc394558f91ba32e0ec5b4
swc0620 <pinetreewave@naver.com>
2022-03-13 19:35:00 +0900

    241. ES6 Import Statements

--- basic_react/src/index.js
+++ basic_react/src/index.js
@@ -1,7 +1,9 @@
+import { React } from 'react';
+
 // Create a new component. This component should produce some HTML
 const App = () => {
     return <div>Hi</div>;
 }
 
 // Take this conponent's generated HTML and put it on the page (in the DOM)
-
+React.render(App);
