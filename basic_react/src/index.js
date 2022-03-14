374c48ebd2f8795dba38ba8c3a4d44ee45124b3e
swc0620 <pinetreewave@naver.com>
2022-03-14 20:28:38 +0900

    254. Youtube Search Response

--- basic_react/src/index.js
+++ basic_react/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 let API_KEY;
+
+YTSearch({ key: API_KEY, term: 'surfboards'}, (data) => {
+    console.log(data);
+});
 
 // Create a new component. This component should produce some HTML
 const App = () => {
