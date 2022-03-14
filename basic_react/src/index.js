2af9d1ba14f6846fd705022240815d0d5480572f
swc0620 <pinetreewave@naver.com>
2022-03-14 20:52:37 +0900

    255. Refactoring Functional Components to Class Components

--- basic_react/src/index.js
+++ basic_react/src/index.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import { API_KEY } from "../config.js"
 
-let API_KEY;
 
-YTSearch({ key: API_KEY, term: 'surfboards'}, (data) => {
-    console.log(data);
-});
+class App extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = { videos: [] };
 
-// Create a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+        YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );   
+    };
 };
 
-// Take this conponent's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
