f5c0d9e013d676d32feacb2c48c14b468eeab47d
swc0620 <pinetreewave@naver.com>
2022-03-14 19:56:53 +0900

    250. Introduction to State

--- basic_react/src/components/search_bar.js
+++ basic_react/src/components/search_bar.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchBar = () => {
+    const [term, setTerm] = useState();
+
     const onInputChange = (event) => {
         console.log(event.target.value)
-    }
+    };
+    
     return <input onChange={ onInputChange } />;
 };
 
