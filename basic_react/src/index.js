b20f3628bf3e31b847c131a8fae11d5c5ea8a8a2
swc0620 <pinetreewave@naver.com>
2022-03-14 10:55:29 +0900

    244. Render Targets

--- basic_react/src/index.js
+++ basic_react/src/index.js
@@ -1,10 +1,10 @@
-import { React } from 'react';
-import { ReactDOM } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 // Create a new component. This component should produce some HTML
 const App = () => {
-    return <div>Hi</div>;
+    return <div>Hi!</div>;
 }
 
 // Take this conponent's generated HTML and put it on the page (in the DOM)
-ReactDOM.render(<App />);
+ReactDOM.render(<App />, document.querySelector('.container'));
