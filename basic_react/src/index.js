c51f2a830eb190f0dc091cdb7e7f157c82abe8bd
swc0620 <pinetreewave@naver.com>
2022-03-14 19:22:25 +0900

    246. Youtube Search API Signup

--- basic_react/src/index.js
+++ basic_react/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+let API_KEY;
+
 // Create a new component. This component should produce some HTML
 const App = () => {
     return <div>Hi!</div>;
