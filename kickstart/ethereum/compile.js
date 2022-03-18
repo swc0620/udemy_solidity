45c0af65f24e1c190613d858c680bbb93984ff48
swc0620 <pinetreewave@naver.com>
2022-03-18 16:38:44 +0900

    149. More on Compile

---
+++ kickstart/ethereum/compile.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const solc = require('solc');
+const fs = require('fs-extra');
+
+// Delete entire 'build' folder
+const buildPath = path.resolve(__dirname, 'build');
+fs.removeSync(buildPath);
+
+// Read 'Campaign.sol from the 'contracts' folder
+const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
+const source = fs.readFileSync(campaignPath, 'utf8');
+
+// Compile both contracts with solidity compiler
+const output = solc.compile(source, 1).contracts;
+
+// Write output to the 'build' directory
+fs.ensureDirSync(buildPath);
+for (let contract in output) {
+    fs.outputJsonSync(path.resolve(buildPath, contract.slice(1)+'.json'), output[contract]);
+};
