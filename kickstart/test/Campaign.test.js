138dbf87ff775b68567c487226b0c2e435a916a3
swc0620 <pinetreewave@naver.com>
2022-03-21 14:49:38 +0900

    153. Accessing Mappings

--- kickstart/test/Campaign.test.js
+++ kickstart/test/Campaign.test.js
@@ -21,5 +21,27 @@ beforeEach(async () => {
     await factory.methods.createCampaign('100').send({ from: accounts[0], gas: '1000000' });
 
     [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
-    campaign = await web3.eth.Contract(JSON.parse(compiledCampaign.interface, campaignAddress));
+    campaign = await new web3.eth.Contract(JSON.parse(compiledCampaign.interface), campaignAddress);
+});
+
+describe('Campaigns', () => {
+    it('deploys a factory and a campaign', () => {
+        assert.ok(factory.options.address);
+        assert.ok(campaign.options.address);
+    });
+
+    it('marks caller as the campaign manager', async () => {
+        const manager = await campaign.methods.manager().call();
+        assert.equal(accounts[0], manager);
+    });
+
+    it('allows people to contrivute money and marks them as approvers', async () => {
+        await campaign.methods.contribute().send({
+            value: '200',
+            from: accounts[1]
+        });
+
+        const isContributor = await campaign.methods.approvers(accounts[1]).call();
+        assert(isContributor);
+    });
 });
