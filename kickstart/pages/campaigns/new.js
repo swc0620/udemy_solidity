22392e2f6678e6b89f92221bc8cc4b717c61e34c
swc0620 <pinetreewave@naver.com>
2022-03-25 17:12:16 +0900

    190. Form Error Handling

--- kickstart/pages/campaigns/new.js
+++ kickstart/pages/campaigns/new.js
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
-import { Form, Button, Input } from 'semantic-ui-react';
+import { Form, Button, Input, Message } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
     state = {
-        minimumContribution: ''
+        minimumContribution: '',
+        errorMessage: ''
     };
 
     onSubmit = async (event) => {
         event.preventDefault();
 
-        const accounts = await web3.eth.getAccounts();
-        await factory.methods
-            .createCampaign(this.state.minimumContribution)
-            .send({
-                from: accounts[0]
-            });
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await factory.methods
+                .createCampaign(this.state.minimumContribution)
+                .send({
+                    from: accounts[0]
+                });
+        } catch (err) {
+            this.setState({errorMessage: err.message});
+        };
     };
 
     render() {
@@ -25,7 +30,7 @@ class CampaignNew extends Component {
             <Layout>
                 <h1>Create a Campaign!</h1>
 
-                <Form onSubmit={this.onSubmit}>
+                <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
                     <Form.Field>
                         <label>Minimum Contribution</label>
                         <Input 
@@ -35,6 +40,8 @@ class CampaignNew extends Component {
                             onChange={event => this.setState({minimumContribution: event.target.value})}
                         />
                     </Form.Field>
+
+                    <Message error header="Oops!" content={this.state.errorMessage} />
 
                     <Button primary>Create!</Button>
                 </Form>
