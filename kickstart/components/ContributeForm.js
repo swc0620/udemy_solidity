3d08b793943cfbd7013a30147bd302af7183991d
swc0620 <pinetreewave@naver.com>
2022-03-26 02:36:56 +0900

    207. The Contribute Form

---
+++ kickstart/components/ContributeForm.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Form, Input, Message, Button } from 'semantic-ui-react';
+
+class ContributionForm extends Component {
+    render() {
+        return (
+            <Form>
+                <Form.Field>
+                    <label>Amount to Contribute</label>
+                    <Input label="ether" labelPosition="right" />
+                </Form.Field>
+                <Button primary>
+                    Contribute!
+                </Button>
+            </Form>
+        );
+    };
+}
+
+export default ContributionForm;
--- kickstart/pages/campaigns/show.js
+++ kickstart/pages/campaigns/show.js
@@ -3,6 +3,7 @@ import { Card } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
 import web3 from '../../ethereum/web3';
+import ContributionForm from '../../components/ContributeForm';
 
 class CampaignShow extends Component {
     static async getInitialProps(props) {
@@ -60,6 +61,7 @@ class CampaignShow extends Component {
             <Layout>
                 <h3>Campaign Show</h3>
                 {this.renderCards()}
+                <ContributionForm />
             </Layout>
         );
     };
