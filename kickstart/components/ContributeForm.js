e5c65abbc841aa25b3eafaa993a15cb29d9c1673
swc0620 <pinetreewave@naver.com>
2022-03-26 02:40:26 +0900

    209. Form State

--- kickstart/components/ContributeForm.js
+++ kickstart/components/ContributeForm.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import { Form, Input, Message, Button } from 'semantic-ui-react';
 
 class ContributionForm extends Component {
+    state = {
+        value: ''
+    };
+
+    onSubmit = (event) => {
+        event.preventDefault();
+    };
+
     render() {
         return (
-            <Form>
+            <Form onSubmit={this.onSubmit}>
                 <Form.Field>
                     <label>Amount to Contribute</label>
-                    <Input label="ether" labelPosition="right" />
+                    <Input
+                        value={this.state.value}
+                        onChange={event => this.setState({value: event.target.value})}
+                        label="ether" 
+                        labelPosition="right" 
+                    />
                 </Form.Field>
                 <Button primary>
                     Contribute!
