9cb135d3a7b0a1056674a094d013449f3b9519b2
swc0620 <pinetreewave@naver.com>
2022-03-26 01:09:21 +0900

    202. CampaignShow's GetInitialProps

--- kickstart/ethereum/factory.js
+++ kickstart/ethereum/factory.js
@@ -1,7 +1,7 @@
 import web3 from './web3';
 import CampaignFactory from './build/CampaignFactory.json';
 
-const CAMPAIGNFACTORY_CONTRACT_ADDRESS = '0x14cc33f03C1C36aD06Ec3B88C58B9f135Cd74faD';
+const CAMPAIGNFACTORY_CONTRACT_ADDRESS = '0xF2D17d1811281b2173DE48186cCf80707873d701';
 
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
